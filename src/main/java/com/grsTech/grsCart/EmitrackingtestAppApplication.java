fd7e2b2ebe2fd0dce7c689eb024439dad8ec648a
siddhu.sg92@gmail.com <siddhu.sg92@gmail.com>
2023-03-22 20:54:54 +0800

    initial commit

---
+++ src/main/java/com/grsTech/grsCart/EmitrackingtestAppApplication.java
@@ -0,0 +1,13 @@
+package com.grsTech.grsCart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmitrackingtestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmitrackingtestAppApplication.class, args);
+	}
+
+}
