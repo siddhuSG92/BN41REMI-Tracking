b74c1389d38e33575795f867386a65266d27d4ff
siddhu.sg92@gmail.com <siddhu.sg92@gmail.com>
2023-03-23 18:00:20 +0800

    added varriable

--- src/main/java/com/grsTech/grsCart/EmitrackingtestAppApplication.java
+++ src/main/java/com/grsTech/grsCart/EmitrackingtestAppApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EmitrackingtestAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmitrackingtestAppApplication.class, args);
+		SpringApplication.run(EmitrackingtestAppApplication.class, args);	
+		int var = 10 ;
 	}
 
 }
