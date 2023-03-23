14d2240e4af53d0f0ecdc9414547e788f5278159
siddhu.sg92@gmail.com <siddhu.sg92@gmail.com>
2023-03-23 18:18:04 +0800

    Gira-1221 changes has been done

--- src/main/java/com/grsTech/grsCart/EmitrackingtestAppApplication.java
+++ src/main/java/com/grsTech/grsCart/EmitrackingtestAppApplication.java
@@ -6,10 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EmitrackingtestAppApplication {
 
+	private Static final String REST_URL = "https://www.grsTech.com/getDetails"
+
 	public static void main(String[] args) {
 		SpringApplication.run(EmitrackingtestAppApplication.class, args);	
 		int var = 10 ;
 		int ra = 100 ;
+	  	int a = 10 ;
 	}
-
 }
