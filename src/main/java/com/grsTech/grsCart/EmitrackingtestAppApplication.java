bd95f7ec55f79bec33cb4b6d8f128005fdc79a8b
siddhu.sg92@gmail.com <siddhu.sg92@gmail.com>
2023-03-23 18:23:13 +0800

    Gira 1222 fixed

--- src/main/java/com/grsTech/grsCart/EmitrackingtestAppApplication.java
+++ src/main/java/com/grsTech/grsCart/EmitrackingtestAppApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EmitrackingtestAppApplication {
+	private Map<String, Object> map = new HashMap<String, Object>
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmitrackingtestAppApplication.class, args);	
 		int var = 10 ;
 		int ra = 100 ;
+		String kk = "Test" ;
 	}
 
 }
